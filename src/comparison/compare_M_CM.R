041c14f7344039df50a2a48915a1aad9e4de843a
Vincent Labatut <vince.labatut@gmail.com>
2020-05-21 15:04:53 +0200

    update CM/M comparison script

--- src/comparison/compare_M_CM.R
+++ src/comparison/compare_M_CM.R
@@ -36,48 +36,69 @@ m.data <- load.m.data(out.folder=FOLDER_OUT_CM, correct.data, complete.data)
 # load the municipal data
 tlog(0,"Load municipal data")
 cm.data <- load.cm.data(out.folder=FOLDER_OUT_M, correct.data, complete.data)
-#
-#
-#
-#
-##############################################################################################
-## compare rows
-#sel.cols <- c(
-#	COL_ATT_ELU_ID,
+# keep only the mayors
+cm.data <- cm.data[which(cm.data[,COL_ATT_FCT_NOM]=="MAIRE"),]
+tlog(0,"Comparing ",nrow(m.data)," (M) vs. ",nrow(cm.data), " (CM)")
+
+
+
+
+#############################################################################################
+# compare rows exactly
+tlog(0,"Performing exact comparison")
+sel.cols <- c(
+	COL_ATT_ELU_ID,
 #	COL_ATT_ELU_NOM,
 #	COL_ATT_ELU_PRENOM,
 #	COL_ATT_ELU_NAIS_DATE,
 #	COL_ATT_ELU_SEXE,
-#	COL_ATT_MDT_DBT,
-#	COL_ATT_MDT_FIN
-#)
-#cm.codes <- future_apply(cm.data[,sel.cols],1,function(r) paste(r,collapse=":"))
-#m.codes <- future_apply(m.data[,sel.cols],1,function(r) paste(r,collapse=":"))
-## look for all mayors in the municipal data
-#idx <- match(m.codes,cm.codes)
-#unmatched <- which(is.na(idx))
-#tlog(2,"Unmatched rows of M:")
-#print(m.codes[unmatched])
-#
-#
-#
-#
-##############################################################################################
-## look for unmatched persons
-#sapply(unmatched, function(um)
-#{	tlog(2, "Processing unmatched mayor")
-#	print(m.data[um,sel.cols])
-#	tlog(4, "Municipal councilors with the same name:")
-#	idx <- which(cm.data[,COL_ATT_ELU_NOM]==m.data[um,COL_ATT_ELU_NOM])
-#	print(cm.data[idx,sel.cols])
-#})
-#
-#
-#
-#
+	COL_ATT_MDT_DBT,
+	COL_ATT_MDT_FIN
+)
+cm.codes <- future_apply(cm.data[,sel.cols],1,function(r) paste(r,collapse=":"))
+m.codes <- future_apply(m.data[,sel.cols],1,function(r) paste(r,collapse=":"))
+# look for all mayors in the municipal data
+m.map <- match(m.codes,cm.codes)
+m.unmatched <- which(is.na(m.map))
+tlog(2,"Number of CM rows appearing several times: ",which(table(m.unmatched)>1))
+# and the opposite
+cm.map <- match(cm.codes,m.codes)
+cm.unmatched <- which(is.na(cm.map))
+tlog(2,"Number of M rows appearing several times: ",which(table(cm.unmatched)>1))
+# remove matched rows from tables
+tlog(2,"Found ",length(m.unmatched)," unmatched rows in M and ",length(cm.unmatched)," in CM")
+m.data <- m.data[m.unmatched,]
+cm.data <- cm.data[cm.unmatched,]
+#print(m.codes[m.unmatched])
+
+
+
+
 #############################################################################################
-# more flexible comparison of dates
-map <- match.similar.tables(m.data, cm.data)
+# compare remaining rows in a more flexible way (dates)
+m.map <- match.similar.tables(m.data, cm.data)
+m.unmatched <- which(is.na(m.map))
+cm.map <- match.similar.tables(cm.data, m.data)
+cm.unmatched <- which(is.na(cm.map))
+# remove matched rows from tables
+tlog(2,"Found ",length(m.unmatched)," unmatched rows in M and ",length(cm.unmatched)," in CM")
+m.data <- m.data[m.unmatched,]
+cm.data <- cm.data[cm.unmatched,]
+
+
+
+#############################################################################################
+# look for unmatched persons
+x <- future_sapply(1:nrow(m.data), function(r1)
+{	idx <- which(cm.data[,COL_ATT_ELU_ID]==m.data[r1,COL_ATT_ELU_ID])
+	if(length(idx)>0)
+	{	tlog(2, "Processing unmatched mayor ",m.data[r1,COL_ATT_ELU_ID])
+		tlog(4, format.row(m.data[r1,]))
+		tlog(4, "Municipal councilors with the same id:")
+		for(r2 in idx)
+			tlog(4, format.row(cm.data[r2,]))
+	}
+})
 
 
 
