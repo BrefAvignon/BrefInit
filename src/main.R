17323f9e8aa797615518d5c1b44274d340e5337f
Vincent Labatut <vince.labatut@gmail.com>
2020-06-11 18:26:42 +0200

    updated comments in main

--- src/main.R
+++ src/main.R
@@ -14,26 +14,11 @@ source("src/common/include.R")
 
 
 
-# TODO revoir l'ordre des ids lors de la vérification des homonymes: ordre numérique
-# dans fichiers mais alphabétique dans script, pas compatible.
-
-
 # analyse de séquences
+# - TODO convertir la table pour qu'elle soit exploitable pour l'analyse de séquences
 # - gestion temps/evts
 #   - évt sans accumulation (pas de distinction entre mandats consécutifs)
 #   - evt avec accu (on distingue mandats consécutifs)
 #   - on considère la durée du mandat
 # - mettre le focus sur un poste donné plutot que sur un individu donné
 #   (séquence des personnes ou classes de personnes ayant occupé un poste donné)
-
-# TODO
-# - passer en revue les fichiers de problèmes: *CM
-# - passer en revue les logs: CD, CM, CR, D, DE, EPCI, M, S
-# - faire la fusion et re-tester
-# - traiter la comparaison approchée des noms
-
-# pb repéré sur les figures d'évolution du nbre de mandat *après* corrections
-# >> pb à voir, notamment sur DE et D (S ?)
-# >> pourtant, on trouve dans la BD S ou AN toutes les lignes présentent dans le RNE, donc il ne devrait pas y avoir de pb...
-#    et pr députés, on s'assure de ne pas avoir un poste occupé par plusieurs personnes à la fois...
-
